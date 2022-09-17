5dae4e5d79a5af45a4005c7aa52f2d998985f356
Rod Falanga <rfalanga@users.noreply.github.com>
2022-09-17 07:56:51 -0600

    these changes were made on a feature branch

--- LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/Program.cs
+++ LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/Program.cs
@@ -1,2 +1,5 @@
 ﻿// Committing from my first GitHub account. This should cause a conflict!
+
+// This line is put here so I can test Git stashing, on a feature branch
+
 Console.WriteLine("Hello, World!");
