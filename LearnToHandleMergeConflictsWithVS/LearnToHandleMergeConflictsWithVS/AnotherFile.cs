dcdbcc77b54e2713bbfc53cd8742c317fff87064
Rod Falanga <rfalanga@users.noreply.github.com>
2025-09-12 19:21:48 -0600

    Upgraded projkect to .NET 9
    Adderd AnotherFIle.cs

---
+++ LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/AnotherFile.cs
@@ -0,0 +1,9 @@
+﻿namespace LearnToHandleMergeConflictsWithVS
+{
+    internal class AnotherFile
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
