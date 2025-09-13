3bbafac9ebf8598d79343a83479b2ab0a8ff0701
Rod Falanga <rfalanga@users.noreply.github.com>
2025-09-13 06:36:24 -0600

    switched about 2 lines

--- LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/AnotherFile.cs
+++ LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/AnotherFile.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; } = string.Empty;   // I want to keep this change, and remove the other one from the child branch
         public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
     }
 }
