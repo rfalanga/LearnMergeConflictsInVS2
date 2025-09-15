c314a9078fc2274b671e1800f8290445564e65b5
Rod Falanga <rfalanga@users.noreply.github.com>
2025-09-15 16:48:14 -0600

    Some more changes to make things interesting

--- LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/AnotherFile.cs
+++ LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/AnotherFile.cs
@@ -3,10 +3,9 @@
     internal class AnotherFile
     {
         public int Id { get; set; }
-        public string FirstName { get; set; } = string.Empty;   // I want to keep this change, and remove the other one from the child branch
         public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; } = string.Empty;
-        public DateTime DateOfBirth { get; set; }
     }
 }
--- LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/Program.cs
+++ LearnToHandleMergeConflictsWithVS/LearnToHandleMergeConflictsWithVS/Program.cs
@@ -8,4 +8,7 @@
 Console.WriteLine("Hello, World");  // I want to keep this change from the child branch
 
 int x = 5;  //ignore the fact that these two variables are unused
-int y = 10; 
+int y = 10;
+
+string aString = "Some other string";
+string bString = "yet another string";
